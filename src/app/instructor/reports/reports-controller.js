034d359d6f85cec659e18aab7bb1edad6feeaa76
Gregg Hartling <hartling@gmail.com>
2014-08-06 18:08:39 -0700

    Make dropdowns at the top dynamic, preload games and classes, set a watch on the dropdowns that will load requested data.

--- src/app/instructor/reports/reports-controller.js
+++ src/app/instructor/reports/reports-controller.js
@@ -14,9 +14,12 @@ angular.module( 'instructor.reports', [
       }
     },
     resolve: {
-      // gameDetails: function($stateParams, GamesService) {
-      //   return GamesService.getDetail($stateParams.gameId);
-      // }
+      allGames: function(GamesService) {
+        return GamesService.all();
+      },
+      courses: function(CoursesService) {
+        return CoursesService.getActiveEnrollmentsWithStudents();
+      }
     },
     data: {
       authorizedRoles: ['instructor'],
@@ -27,45 +30,102 @@ angular.module( 'instructor.reports', [
 
 
 .controller( 'ReportsCtrl',
-  function($scope, $state, $stateParams, $log, CoursesService, ReportsService) {
+  function($scope, $state, $stateParams, $log, allGames, courses, CoursesService, ReportsService) {
 
-    $scope.courses = [];
+    $scope.games = {
+      isOpen: false,
+      selected: 'AA-1',
+      options: {}
+    };
+    angular.forEach(allGames, function(game) {
+      $log.info(game);
+      if (game.enabled) {
+        $scope.games.options[''+game.gameId] = game;
+      }
+    });
+
+
+    $scope.reports = {
+      isOpen: false,
+      selected: 'SOWO',
+      options: {
+        'SOWO': {
+          id: 'SOWO',
+          title: 'Shout Out and Watch Out',
+          positon: 1
+        },
+        'ACHV': {
+          id: 'ACHV',
+          title: 'Game Achievements & Time Played',
+          position: 2
+        }
+      }
+    };
+
+    $scope.selectGame = function($event, key) {
+      $scope.games.selected = key;
+      $scope.toggleDropdown($event, 'games');
+    };
+
+    $scope.selectReport = function($event, key) {
+      $scope.reports.selected = key;
+      $scope.toggleDropdown($event, 'reports');
+    };
+
+    $scope.toggleDropdown = function($event, collection) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope[collection].isOpen = !$scope[collection].isOpen;
+    };
+
+
+
+
+    $scope.courses = courses;
+
+    $scope.$watchCollection('[games.selected, reports.selected]', function(newValue, oldValue) {
+      $log.info(oldValue);
+      $log.info(newValue);
+    });
+
+
+    // $scope.courses = [];
     $scope.students = [];
     $scope.achievements = [];
     $scope.activeAchievements = [];
 
-    CoursesService.getEnrollmentsWithStudents()
-      .then(function(data) {
-        $log.info(data);
-        angular.forEach(data, function(course) {
-          if (!course.archived) {
-            $scope.courses.push(course);
-          }
-        });
-        angular.forEach($scope.courses[0].users, function(student) {
-          $scope.students.push(student);
-        });
-        $scope.courses[0].isExpanded = true;
-        ReportsService.getAchievements('AA-1', $scope.courses[0].id)
-          .then(function(students) {
-            angular.forEach(students, function(student) {
-              angular.forEach($scope.students, function(s) {
-                if (s.id == student.userId) {
-                  s.achievements = student.achievements;
-                }
-              });
-            });
-            angular.forEach($scope.students[0].achievements, function (achievement) {
-              var obj = angular.copy(achievement);
-              delete obj.won;
-              $scope.achievements.push(obj);
-            });
-            $scope.activeAchievements.push($scope.achievements[0]);
-            $scope.activeAchievements.push($scope.achievements[1]);
-            $scope.activeAchievements.push($scope.achievements[2]);
-            $log.info($scope.activeAchievements);
-          });
-      });
+    // CoursesService.getEnrollmentsWithStudents()
+    //   .then(function(data) {
+    //     $log.info(data);
+    //     angular.forEach(data, function(course) {
+    //       if (!course.archived) {
+    //         $scope.courses.push(course);
+    //       }
+    //     });
+    //     angular.forEach($scope.courses[0].users, function(student) {
+    //       $scope.students.push(student);
+    //     });
+    //     $scope.courses[0].isExpanded = true;
+    //     ReportsService.getAchievements('AA-1', $scope.courses[0].id)
+    //       .then(function(students) {
+    //         angular.forEach(students, function(student) {
+    //           angular.forEach($scope.students, function(s) {
+    //             if (s.id == student.userId) {
+    //               s.achievements = student.achievements;
+    //             }
+    //           });
+    //         });
+    //         angular.forEach($scope.students[0].achievements, function (achievement) {
+    //           var obj = angular.copy(achievement);
+    //           delete obj.won;
+    //           $scope.achievements.push(obj);
+    //         });
+    //         $scope.activeAchievements.push($scope.achievements[0]);
+    //         $scope.activeAchievements.push($scope.achievements[1]);
+    //         $scope.activeAchievements.push($scope.achievements[2]);
+    //         $log.info($scope.activeAchievements);
+    //       });
+    //   });
 
 
   $scope.getStudentResult = function(studentId, achievement) {
