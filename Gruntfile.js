f0fce190862a05692682e76498bd47eddf2e4809
Michael Mulligan <michael.mulligan@instituteofplay.org>
2014-08-25 17:08:46 -0700

    - removed protractor from build instructions - requires selenium standalone to be running, use `grunt mocha` instead.

--- Gruntfile.js
+++ Gruntfile.js
@@ -587,7 +587,7 @@ module.exports = function ( grunt ) {
     'clean', 'html2js', 'jshint', 'less:build',
     'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
     'copy:build_appjs', 'copy:build_vendorjs', 'copy:crossdomain', 'copy:favicon', 'index:build', 'karmaconfig',
-    'karma:continuous', 'protractor:build',
+    'karma:continuous',
     'createVersionFile'
   ]);
     
