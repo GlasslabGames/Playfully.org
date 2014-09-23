1fbf8c75dc4888c4bd3e89c64771b2c302aea028
Charles Tai <charles@glasslabgames.org>
2014-09-23 08:53:21 -0700

    Grunt file tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -488,7 +488,7 @@ module.exports = function ( grunt ) {
         files: [ 
           '<%= app_files.js %>'
         ],
-        tasks: [ 'jshint:src', 'copy:build_appjs' ]
+        tasks: [ 'jshint:src', 'karma:unit:run', 'copy:build_appjs' ]
       },
 
 
