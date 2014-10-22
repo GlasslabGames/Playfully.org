568d2fc62119e085f54e824919306630beccf4f9
Charles Tai <charles@glasslabgames.org>
2014-10-22 14:03:38 -0700

    fixed disabled arrow mission progress

--- src/app/instructor/reports/reports.const.js
+++ src/app/instructor/reports/reports.const.js
@@ -2,11 +2,11 @@ angular.module('reports.const', [])
 
 .constant('REPORT_CONSTANTS', {
  legend: {
-  'CCSS.ELA-Literacy.WHST.6-8.1a': 'blue-square',
-  'CCSS.ELA-Literacy.WHST.6-8.1b': 'green-square',
-  'Problem Solving': 'red-square',
-  'Citizenship': 'yellow-square',
-  'CCSS.ELA-Literacy.RI.6-8': 'light-blue-square'
+  'CCSS.ELA-Literacy.WHST.6-8.1a': 'gl-reports-achievement-blue-square',
+  'CCSS.ELA-Literacy.WHST.6-8.1b': 'gl-reports-achievement-green-square',
+  'Problem Solving': 'gl-reports-achievement-red-square',
+  'Citizenship': 'gl-reports-achievement-yellow-square',
+  'CCSS.ELA-Literacy.RI.6-8': 'gl-reports-achievement-light-blue-square'
  }
 
 });
