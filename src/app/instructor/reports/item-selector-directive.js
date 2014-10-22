caf83f1a84f3bab7990f910585444b8ea4564bfd
Charles Tai <charles@glasslabgames.org>
2014-10-22 16:51:13 -0700

    moved selectStudents and getSelectetedStudentIds into its own Service

--- src/app/instructor/reports/item-selector-directive.js
+++
@@ -1,6 +0,0 @@
-angular.module('instructor.reports')
-    .directive('itemSelector', function() {
-        return {
-            templateUrl: 'instructor/reports/item-selector-directive.html'
-        };
-});
