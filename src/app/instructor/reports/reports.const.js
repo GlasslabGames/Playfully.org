d97d4dfe056784e3c55a3383c05f525c31c5970a
Charles Tai <charles@glasslabgames.org>
2014-11-07 11:06:50 -0800

    competency not-covered css change, added hover description as well

--- src/app/instructor/reports/reports.const.js
+++ src/app/instructor/reports/reports.const.js
@@ -9,10 +9,10 @@ angular.module('reports.const', [])
   'Citizenship': 'gl-reports-achievement-yellow-square',
   'CCSS.ELA-Literacy.RI.6-8': 'gl-reports-achievement-light-blue-square',
   // Competency
-  'mastered': 'gl-reports-competency-circle-green',
-  'not-mastered': 'gl-reports-competency-circle-red',
-  'not-enough-info': 'gl-reports-competency-circle-yellow',
-  'not-covered': 'gl-reports-competency-circle-gray'
+  'mastered': {class:'gl-reports-competency-circle-green',text: 'Mastered'},
+  'not-mastered': {class:'gl-reports-competency-circle-red',text: 'Not Mastered'},
+  'not-enough-info': {class:'gl-reports-competency-circle-gray',text: 'Not Enough Info'},
+  'not-covered': {class:'gl-reports-competency-circle-white',text: 'Not Covered'}
  }
 
 });
