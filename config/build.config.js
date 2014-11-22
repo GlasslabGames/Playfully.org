06a2424fa7c82c94ae2260452c469b78103f631a
Charles Tai <charles@glasslabgames.org>
2014-11-21 21:17:35 -0800

    Style Dashboard

--- config/build.config.js
+++ config/build.config.js
@@ -78,7 +78,8 @@ module.exports = {
       'vendor/sticky-ng/js/stickyNg.js',
       'vendor/sticky-ng/js/jquery.stickytableheaders.js',
       'vendor/lodash/dist/lodash.min.js',
-      'vendor/angular-local-storage/dist/angular-local-storage.min.js'
+      'vendor/angular-local-storage/dist/angular-local-storage.min.js',
+      'vendor/ui-router-extras/release/ct-ui-router-extras.min.js'
     ],
     css: [
       'vendor/bootstrap/dist/css/bootstrap.min.css',
--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,5 @@
 angular.module( 'playfully', [
+  'ct.ui.router.extras',
   'ngOrderObjectBy',
   'ngSanitize',
   'ipCookie',
