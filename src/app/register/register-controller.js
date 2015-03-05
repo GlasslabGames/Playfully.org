02b35013c579401ee7a74ce193852f00d37eab3b
Charles Tai <charles@glasslabgames.org>
2015-03-05 12:36:15 -0800

    Added - registering through trial flow starts trial on login

--- src/app/register/register-controller.js
+++ src/app/register/register-controller.js
@@ -110,13 +110,13 @@ angular.module('playfully.register', ['register.const'])
 
 .controller('RegisterInstructorCtrl',
     function ($scope, $log, $rootScope, $state, $stateParams, $window, UserService, Session, AUTH_EVENTS, ERRORS, REGISTER_CONSTANTS) {
-      var user = null;
-      var upgrade = null || $stateParams.upgrade;
-      var packageInfo = {
-          packageType: null || $stateParams.packageType,
-          seatsSelected: null || $stateParams.seatsSelected
-      };
-      $scope.account = {
+        var user = null;
+        var packageInfo = {
+            packageType: null || $stateParams.packageType,
+            seatsSelected: null || $stateParams.seatsSelected
+         };
+        $scope.upgrade = null || $stateParams.upgrade;
+        $scope.account = {
         firstName: '',
         lastName: '',
         email: '',
@@ -134,6 +134,13 @@ angular.module('playfully.register', ['register.const'])
       $scope.states = REGISTER_CONSTANTS.states;
 
       $scope.register = function( account ) {
+        if ($scope.upgrade === 'trial') {
+            var emailAddress = $scope.account.email.split('@')[0].indexOf('+');
+            if (emailAddress >= 0) {
+                $scope.account.errors.push('Trial accounts cannot have the + symbol in their email address');
+                return;
+            }
+        }
         $scope.regForm.isSubmitting = true;
         if (account.firstName && account.firstName.indexOf(' ') > -1) {
           firstName = account.firstName.substr(0, account.firstName.indexOf(' '));
@@ -147,7 +154,7 @@ angular.module('playfully.register', ['register.const'])
         else {
           account.standards = "CCSS";
         }
-        UserService.register(account, upgrade, packageInfo)
+        UserService.register(account, $scope.upgrade, packageInfo)
           .success(function(data, status, headers, config) {
             user = data;
             Session.create(user.id, user.role, data.loginType);
