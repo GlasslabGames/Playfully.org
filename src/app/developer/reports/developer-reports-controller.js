8223bfa35a68293fd21328c74eac0eb886a74627
Kenneth Yeast <kenneth.yeast@gmail.com>
2015-12-28 20:29:40 -0800

    DAUU report without connection

--- src/app/developer/reports/developer-reports-controller.js
+++ src/app/developer/reports/developer-reports-controller.js
@@ -1,4 +1,4 @@
-angular.module('developer.reports', [])
+angular.module('developer.reports', ['nvd3'])
 
 .config(function($stateProvider) {
     $stateProvider.state('root.developerReports', {
@@ -87,5 +87,93 @@ angular.module('developer.reports', [])
 .controller('DeveloperDetailsCtrl', function ($scope, $stateParams, $http, $window, $state, $sce, ResearchService, gameInfo, infoSchema) {
     $scope.gameId = $stateParams.gameId;
     $scope.fullData = gameInfo;
+
+    $scope.options = {
+        chart: {
+            type: 'lineChart',
+            height: 450,
+            margin : {
+                top: 20,
+                right: 20,
+                bottom: 40,
+                left: 55
+            },
+            x: function(d){ return d.x; },
+            y: function(d){ return d.y; },
+            useInteractiveGuideline: true,
+            dispatch: {
+                stateChange: function(e){ console.log("stateChange"); },
+                changeState: function(e){ console.log("changeState"); },
+                tooltipShow: function(e){ console.log("tooltipShow"); },
+                tooltipHide: function(e){ console.log("tooltipHide"); }
+            },
+            xAxis: {
+                axisLabel: 'Date',
+                tickFormat: function(d){
+                	if ( d === 0 ) {
+                		return "today";
+                	}
+                	else {
+						var date = new Date(Date.now());
+						date.setDate(date.getDate()+d);
+						var options = { month: 'short', day: '2-digit' };
+                        return (date.toLocaleDateString( 'en-US', options ));
+                    }
+                }
+            },
+            yAxis: {
+                axisLabel: 'Daily Active Unique Users',
+                tickFormat: function(d){
+                    return d3.format('f')(d*100.0);
+                },
+                axisLabelDistance: -10
+            },
+            callback: function(chart){
+                //console.log("!!! lineChart callback !!!");
+            }
+        },
+        title: {
+            enable: true,
+            text: 'Daily Active Unique Users'
+        },
+        subtitle: {
+            enable: false,
+            text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+            css: {
+                'text-align': 'center',
+                'margin': '10px 13px 0px 7px'
+            }
+        },
+        caption: {
+            enable: false,
+            html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
+            css: {
+                'text-align': 'justify',
+                'margin': '10px 13px 0px 7px'
+            }
+        }
+    };
+
+    $scope.data = sinAndCos();
+
+    /*Random Data Generator */
+    function sinAndCos() {
+        var sin = [];
+
+        //Data is represented as an array of {x,y} pairs.
+        for (var i = -31; i <= 0; i++) {
+            sin.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
+        }
+
+        //Line chart data should be sent as an array of series objects.
+        return [
+            {
+                values: sin,
+                key: 'Active Unique Users',
+                color: '#7777ff',
+                area: true      //area - set to true if you want this line to turn into a filled area chart.
+            }
+        ];
+    }
 
 });
