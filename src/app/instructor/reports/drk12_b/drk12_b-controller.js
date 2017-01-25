2e4b749fd792410c0d6efd23c96c5ede36d9cfcf
sigh <sigh@concentricsky.com>
2017-01-25 13:59:23 -0800

    DRK-65 Added skill scrolling in modal

--- src/app/instructor/reports/drk12_b/drk12_b-controller.js
+++ src/app/instructor/reports/drk12_b/drk12_b-controller.js
@@ -37,6 +37,40 @@ angular.module( 'instructor.reports')
 
                                 $scope.skills = drk12_bStore.getSkills();
                                 $scope.selectedSkill = drk12_bStore.getSelectedSkill();
+
+                                $scope.isFirstSkill = function(selectedSkillName) {
+                                    return Object.keys($scope.skills)[0] === selectedSkillName;
+                                };
+
+                                $scope.isLastSkill = function(selectedSkillName) {
+                                    return Object.keys($scope.skills)[_.size($scope.skills)-1] === selectedSkillName;
+                                };
+
+                                $scope.incrementSkill = function() {
+                                    var index = 0;
+                                    for(var skillName in $scope.skills) {
+                                        console.info('skillName: ', skillName);
+                                        if (skillName === $scope.selectedSkill) {
+                                            $scope.selectedSkill = Object.keys($scope.skills)[index + 1];
+                                            console.info('selectedSkill = ', $scope.selectedSkill);
+                                            break;
+                                        }
+                                        index++;
+                                    }
+                                };
+
+                                $scope.decrementSkill = function() {
+                                    var index = 0;
+                                    for(var skillName in $scope.skills) {
+                                        console.info('skillName: ', skillName);
+                                        if (skillName === $scope.selectedSkill) {
+                                            $scope.selectedSkill = Object.keys($scope.skills)[index - 1];
+                                            console.info('selectedSkill = ', $scope.selectedSkill);
+                                            break;
+                                        }
+                                        index++;
+                                    }
+                                };
                             }
                         }
                     }
