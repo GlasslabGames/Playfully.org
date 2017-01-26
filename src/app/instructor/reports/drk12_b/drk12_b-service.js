0adc6e61573235cfc3c00fbbc13af9c97bc9c34a
sigh <sigh@concentricsky.com>
2017-01-26 14:19:45 -0800

    DRK-62 Additional simple clean-up. Breaking up controller file.

---
+++ src/app/instructor/reports/drk12_b/drk12_b-service.js
@@ -0,0 +1,40 @@
+angular.module( 'instructor.reports')
+    .service('drk12_bStore', function() {
+        this.currentStudentsArray = [];
+        this.skills = null;
+        this.selectedSkill = "all";
+
+        this.setCurrentStudents = function(currentStudentsArray) {
+            if (currentStudentsArray) {
+                this.currentStudentsArray = currentStudentsArray;
+            }
+        };
+        // Make sure this is pass-by-value
+        this.getCurrentStudents = function() {
+            return angular.copy(this.currentStudentsArray);
+        };
+
+        this.setSkills = function(skills) {
+            this.skills = skills;
+        };
+
+        this.getSkills = function() {
+            return this.skills;
+        };
+
+        this.setSelectedSkill = function(selectedSkill) {
+            if ((!selectedSkill || selectedSkill === "all") && this.skills) {
+                selectedSkill = Object.keys(this.skills)[0];
+            }
+
+            this.selectedSkill = selectedSkill;
+        };
+
+        this.getSelectedSkill = function() {
+            if (!this.selectedSkill) {
+                this.selectedSkill = Object.keys(this.skills)[0];
+            }
+
+            return this.selectedSkill;
+        };
+    });
