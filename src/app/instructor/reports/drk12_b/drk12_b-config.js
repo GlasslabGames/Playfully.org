fd77ff57268982400f3ce5866c4b74a8a9202a46
sigh <sigh@concentricsky.com>
2017-01-26 18:23:16 -0800

    DRK-65 Start of drilldown portion of modal

--- src/app/instructor/reports/drk12_b/drk12_b-config.js
+++ src/app/instructor/reports/drk12_b/drk12_b-config.js
@@ -4,11 +4,12 @@ angular.module( 'instructor.reports')
             url: '/reports/details/drk12_b/game/:gameId/course/:courseId/:type',
             views: {
                 'modal@': {
-                    templateUrl: function($stateParams, REPORT_CONSTANTS) {
-                        return 'instructor/reports/drk12_b/_modal-' + $stateParams.type + '.html';
+                    templateUrl: function() {
+                        return 'instructor/reports/drk12_b/modal-wrapper.html';
                     },
-                    controller: function($scope, $stateParams, $state, $previousState, drk12_bStore) {
-                        var navigateBackToReport = function() {
+                    controller: function($scope, $stateParams, $state, $previousState) {
+                        $scope.type = $stateParams.type;
+                        $scope.navigateBackToReport = function() {
                             $state.go('root.reports.details.drk12_b', {
                                 gameId: $stateParams.gameId,
                                 courseId: $stateParams.courseId
@@ -17,62 +18,14 @@ angular.module( 'instructor.reports')
 
                         // Some magic from the parent object
                         if (!$previousState.get("modalInvoker").state) {
-                            navigateBackToReport();
+                            $scope.navigateBackToReport();
                         }
 
-                        if ($stateParams.type !== "info") {
-                            var currentStudentsArray = drk12_bStore.getCurrentStudents();
-
-                            if (!currentStudentsArray || currentStudentsArray.length < 1) {
-                                navigateBackToReport();
-                            } else {
-                                $scope.singleUserView = true;
-                                $scope.studentsArray = currentStudentsArray;
-                                if (currentStudentsArray.length === 1) {
-                                    $scope.singleUserView = true;
-                                    $scope.student = currentStudentsArray[0];
-                                } else if (currentStudentsArray.length > 1) {
-                                    $scope.singleUserView = false;
-                                }
-
-                                $scope.skills = drk12_bStore.getSkills();
-                                $scope.selectedSkill = drk12_bStore.getSelectedSkill();
-
-                                $scope.isFirstSkill = function(selectedSkillName) {
-                                    return Object.keys($scope.skills)[0] === selectedSkillName;
-                                };
-
-                                $scope.isLastSkill = function(selectedSkillName) {
-                                    return Object.keys($scope.skills)[_.size($scope.skills)-1] === selectedSkillName;
-                                };
-
-                                $scope.incrementSkill = function() {
-                                    var index = 0;
-                                    for(var skillName in $scope.skills) {
-                                        console.info('skillName: ', skillName);
-                                        if (skillName === $scope.selectedSkill) {
-                                            $scope.selectedSkill = Object.keys($scope.skills)[index + 1];
-                                            console.info('selectedSkill = ', $scope.selectedSkill);
-                                            break;
-                                        }
-                                        index++;
-                                    }
-                                };
-
-                                $scope.decrementSkill = function() {
-                                    var index = 0;
-                                    for(var skillName in $scope.skills) {
-                                        console.info('skillName: ', skillName);
-                                        if (skillName === $scope.selectedSkill) {
-                                            $scope.selectedSkill = Object.keys($scope.skills)[index - 1];
-                                            console.info('selectedSkill = ', $scope.selectedSkill);
-                                            break;
-                                        }
-                                        index++;
-                                    }
-                                };
-                            }
-                        }
+                        $scope.data = {
+                            info: {title: "Help"},
+                            studentInfo: {title: "Performance Over Time"},
+                            drilldown: {title: "Performance Over Time"}
+                        };
                     }
                 }
             }
--- src/app/instructor/reports/drk12_b/drk12_b-controller.js
+++ src/app/instructor/reports/drk12_b/drk12_b-controller.js
@@ -437,4 +437,59 @@ angular.module( 'instructor.reports')
         };
 
         populateCharts();
+    })
+    .controller('Drk12bModalStudentInfo', function($scope, drk12_bStore) {
+        var currentStudentsArray = drk12_bStore.getCurrentStudents();
+
+        if (!currentStudentsArray || currentStudentsArray.length < 1) {
+            $scope.navigateBackToReport();
+        } else {
+            $scope.singleUserView = true;
+            $scope.studentsArray = currentStudentsArray;
+            if (currentStudentsArray.length === 1) {
+                $scope.singleUserView = true;
+                $scope.student = currentStudentsArray[0];
+            } else if (currentStudentsArray.length > 1) {
+                $scope.singleUserView = false;
+            }
+
+            $scope.skills = drk12_bStore.getSkills();
+            $scope.selectedSkill = drk12_bStore.getSelectedSkill();
+
+            $scope.isFirstSkill = function(selectedSkillName) {
+                return Object.keys($scope.skills)[0] === selectedSkillName;
+            };
+
+            $scope.isLastSkill = function(selectedSkillName) {
+                return Object.keys($scope.skills)[_.size($scope.skills)-1] === selectedSkillName;
+            };
+
+            $scope.incrementSkill = function() {
+                var index = 0;
+                for(var skillName in $scope.skills) {
+                    console.info('skillName: ', skillName);
+                    if (skillName === $scope.selectedSkill) {
+                        $scope.selectedSkill = Object.keys($scope.skills)[index + 1];
+                        console.info('selectedSkill = ', $scope.selectedSkill);
+                        break;
+                    }
+                    index++;
+                }
+            };
+
+            $scope.decrementSkill = function() {
+                var index = 0;
+                for(var skillName in $scope.skills) {
+                    console.info('skillName: ', skillName);
+                    if (skillName === $scope.selectedSkill) {
+                        $scope.selectedSkill = Object.keys($scope.skills)[index - 1];
+                        console.info('selectedSkill = ', $scope.selectedSkill);
+                        break;
+                    }
+                    index++;
+                }
+            };
+        }
+    })
+    .controller('Drk12bModalDrilldown', function($scope, drk12_bStore) {
     });
