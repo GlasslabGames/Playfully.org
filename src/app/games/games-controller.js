592efe7f2682480d901c96a8778d65ef30c644c0
sigh <sigh@concentricsky.com>
2017-03-17 11:05:50 -0700

    PLAT-761 Hack to pass course code to TEACHABLEAGENTSPR game

--- src/app/games/games-controller.js
+++ src/app/games/games-controller.js
@@ -808,7 +808,7 @@ $stateProvider.state( 'modal.game-user-mismatch', {
         if (ENV.game_sdkURI) {
         	embed = embed + (embed.indexOf('?') === -1 ? "?" : "&") + "sdkURI=" + ENV.game_sdkURI;
         }
-        if (gameDetails.gameId.toUpperCase() === "GEM" || gameDetails.gameId.toUpperCase() === "GEMAUDIO") {
+        if (gameDetails.gameId.toUpperCase() === "GEM" || gameDetails.gameId.toUpperCase() === "GEMAUDIO" || gameDetails.gameId.toUpperCase() === "TEACHABLEAGENTSPR") {
             var foundCourse = _.find(activeCourses, function(course) { return course.id == $state.params.courseId; });
             if (foundCourse && foundCourse.code) {
                 embed = embed + (embed.indexOf('?') === -1 ? "?" : "&") + "classCode=" + foundCourse.code;
